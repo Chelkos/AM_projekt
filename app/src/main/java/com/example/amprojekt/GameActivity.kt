a043be35b725ed58e22580c97e721aeace45476f
yieldie <kacperkrasnal@gmail.com>
2021-05-18 16:31:30 +0200

    Major changes in gameplay

--- app/src/main/java/com/example/amprojekt/GameActivity.kt
+++ app/src/main/java/com/example/amprojekt/GameActivity.kt
@@ -1,17 +1,19 @@
 package com.example.amprojekt
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
 import java.util.*
 
 class GameActivity : AppCompatActivity() {
 
     private var stage = 0
+    private var helpAvailable = true
     lateinit var questionsIndexes : List<Int>
     lateinit var allQuestions : Array<String>
     lateinit var allAnswers : Array<String>
@@ -20,36 +22,57 @@ class GameActivity : AppCompatActivity() {
     lateinit var correctAnswer : String
     lateinit var questionText : TextView
     lateinit var shuffledAnswers : List<String>
-    lateinit var answerABut : Button
-    lateinit var answerBBut : Button
-    lateinit var answerCBut : Button
-    lateinit var answerDBut : Button
+    lateinit var buttonArray : Array<Button>
+    lateinit var messageView : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
         questionText = findViewById(R.id.questionText)
-        answerABut = findViewById(R.id.answerAButton)
-        answerBBut = findViewById(R.id.answerBButton)
-        answerCBut = findViewById(R.id.answerCButton)
-        answerDBut = findViewById(R.id.answerDButton)
-        val listener = View.OnClickListener {
+        messageView = findViewById(R.id.messageView)
+        buttonArray = arrayOf(findViewById(R.id.answerAButton), findViewById(R.id.answerBButton), findViewById(R.id.answerCButton),
+                findViewById(R.id.answerDButton))
+        val listener = View.OnClickListener { it ->
             val button = it as Button
-            if (button.text == correctAnswer) {
-                stage++
-                Toast.makeText(applicationContext, "Dobrze!", Toast.LENGTH_SHORT).show()
-                shuffleAnswers(chosenAnswers[stage])
-                loadStage(stage)
-            } else {
-                Toast.makeText(applicationContext, "Źle!", Toast.LENGTH_SHORT).show()
+            val text = button.text
+            if (text == correctAnswer) {
+                if(stage < 10) {
+                    messageView.text = "Dobrze!"
+                    Handler().postDelayed({
+                        messageView.text = ""
+                    }, 1*1000)
+                    loadStage()
+                } else {
+                    messageView.text = "WYGRANA"
+                    intent = Intent()
+                    intent.putExtra("score", 10)
+                    setResult(1, intent)
+                    Handler().postDelayed({
+                        finish()
+                    }, 3*1000)
+                }
+            } else if(text != "") {
+                messageView.text = "Źle!"
+                Handler().postDelayed({
+                    messageView.text = "KONIEC GRY"
+                }, 1*1000)
+                intent = Intent()
+                intent.putExtra("score", stage - 1)
+                setResult(1, intent)
+                Handler().postDelayed({
+                    finish()
+                                      }, 3*1000)
+                buttonArray.forEach { it.setOnClickListener {  } }
             }
         }
-        answerABut.setOnClickListener(listener)
-        answerBBut.setOnClickListener(listener)
-        answerCBut.setOnClickListener(listener)
-        answerDBut.setOnClickListener(listener)
+        buttonArray.forEach { it.setOnClickListener(listener) }
+        findViewById<Button>(R.id.removeAnswersButton).setOnClickListener {
+            removeAnswers()
+            helpAvailable = false
+            it.setOnClickListener { }
+        }
         getQuestions()
-        loadStage(stage)
+        loadStage()
     }
 
     private fun shuffleAnswers(answers: String) {
@@ -71,12 +94,32 @@ class GameActivity : AppCompatActivity() {
         shuffleAnswers(chosenAnswers[0])
     }
 
-    private fun loadStage(stage: Int) {
+    private fun loadStage() {
+        shuffleAnswers(chosenAnswers[stage])
         questionText.text = chosenQuestions[stage]
-        answerABut.text = shuffledAnswers[0]
-        answerBBut.text = shuffledAnswers[1]
-        answerCBut.text = shuffledAnswers[2]
-        answerDBut.text = shuffledAnswers[3]
+        buttonArray.forEachIndexed { index, button -> button.text = shuffledAnswers[index] }
+        stage++
+    }
+
+    private fun removeAnswers() {
+        val pos = arrayOf(0, 1, 2, 3)
+        val r = Random(System.currentTimeMillis())
+        var j : Int
+        var tmp : Int
+        var c = 2
+        for (i in 3 downTo 0) {
+            j = r.nextInt(i + 1)
+            tmp = pos[i]
+            pos[i] = pos[j]
+            pos[j] = tmp
+        }
+        for (i in 0..3) {
+            if(buttonArray[pos[i]].text != correctAnswer) {
+                buttonArray[pos[i]].text = ""
+                c--
+            }
+            if(c == 0) break
+        }
     }
 
 }
--- app/src/main/java/com/example/amprojekt/MainActivity.kt
+++ app/src/main/java/com/example/amprojekt/MainActivity.kt
@@ -4,21 +4,26 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.amprojekt.R
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var resultView : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        resultView = findViewById(R.id.resultView)
+        resultView.text = ""
         val newGameButton = findViewById<Button>(R.id.newgameBut)
         val leaderButton = findViewById<Button>(R.id.leaderBut)
         val exitButton = findViewById<Button>(R.id.exitBut)
         newGameButton.setOnClickListener {
             val intent = Intent(applicationContext, GameActivity::class.java)
-            startActivity(intent)
+            startActivityForResult(intent, 1)
         }
         leaderButton.setOnClickListener {
             val intent = Intent(applicationContext, LeaderBoardActivity::class.java)
@@ -29,4 +34,17 @@ class MainActivity : AppCompatActivity() {
             exitProcess(0)
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data != null) {
+            val score = data.getIntExtra("score", 0)
+            if(score < 10) {
+                resultView.text = "Your result: $score correct answers"
+            } else {
+                resultView.text = "You won!"
+            }
+        }
+    }
+
 }
